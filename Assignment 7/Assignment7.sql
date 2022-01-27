331c0056143eaeb31ccad68980d9e2629d954256
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2022-01-27 15:30:20 -0500

    Fix

--- Assignment 7/Assignment7.sql
+++ Assignment 7/Assignment7.sql
@@ -1,97 +1,41 @@
-
-    SELECT 
-        CONCAT(EM.fname, ' ', EM.lname) AS 'Employee',
-        DATEPART(YEAR, EM.hire_date) AS 'Year Hired',
-        DATEDIFF(YEAR, EM.hire_date, GETDATE()) AS 'Years Worked'
-    FROM 
-        dbo.employee AS EM
-    WHERE
-        DATEDIFF(YEAR, EM.hire_date,GETDATE()) > 29
-    ORDER BY
-        [Years Worked] DESC
-
-
-SELECT TOP 3
-    TI.title AS 'Title',
-    ROUND(TI.ytd_sales / TI.price, 0) AS 'Qty Sold',
-    TI.price AS 'Price',
-    TI.ytd_sales 'YTD Sales',
-    TI.price * (ROUND(TI.ytd_sales / TI.price, 0)) AS 'Revenue(year)'
-FROM
-    titles AS TI
-ORDER BY
-    'Qty Sold' DESC
-
-
-
-SELECT TOP (3) 
-    TI.title AS "Book Title:",
-    TI.ytd_sales AS "Qty Sold:",
-    TI.price AS "Selling Price:",
-    TI.price * TI.ytd_sales AS "Revenue:"
-FROM 
-    dbo.titles as TI
-ORDER BY
-    TI.price * TI.ytd_sales DESC
-
-SELECT
-    (Em.fname + ' '+ EM.lname) AS 'Employee:',
-    JO.job_desc AS 'Job Title',
-    EM.hire_date AS 'Date Hired'
-FROM 
-    dbo.employee AS EM,
-    dbo.jobs as JO
-WHERE
-    JO.job_desc = 'Publisher'
-    OR 
-    JO.job_desc = 'Editor'
-    OR
-    JO.job_desc = 'Designer'
-ORDER BY
-    JO.job_desc ASC
-
-
-SELECT
-    CONCAT(EM.fname, ' ', EM.lname) AS 'EMPLOYEE',
-    JO.job_desc AS 'JOB TITLE',
-    EM.hire_date AS 'YEAR HIRED'
-FROM
-    employee AS EM 
-    JOIN jobs AS JO
-    ON EM.job_id = JO.job_id 
-ORDER BY 
-    JO.job_desc ASC
-
-
-
-SELECT
-    CONCAT(EM.fname,' ', EM.lname) AS 'Employee',
-    JO.job_desc AS 'Job Title',
-    CONCAT(DATEPART(MONTH,EM.hire_date),', ', 
-        DATEPART(DAY,EM.hire_date),', ', 
-        DATEPART(year,EM.hire_date)) AS 'Hire Date: MDY'
-FROM
-    dbo.employee AS EM
-    JOIN dbo.jobs AS JO
-    ON JO.job_id = EM.job_id -- PK - FK
-WHERE 
-    JO.job_desc = 'Designer' 
-    OR JO.job_desc = 'Publisher' 
-    OR JO.job_desc = 'Editor'
-ORDER  BY
-    [Employee], [Job Title] ASC
-
-
-
-SELECT
-    CONCAT(EM.fname, ' ', EM.lname) AS 'Employee',
-    JO.job_desc AS 'Job Description',
-    DATEPART(YEAR, EM.hire_date) AS 'Year Hired'
-FROM
-    employee AS EM 
-    JOIN jobs AS JO
-    ON JO.job_id = EM.job_id
-WHERE
-    JO.job_desc IN('Editor', 'Publisher', 'Designer')
-ORDER BY
-    JO.job_desc ASC
+CREATE DATABASE Movies_Databases;
+USE msdb
+GO
+	CREATE TABLE Table_Actorsss (
+		Actor_id INT PRIMARY KEY NOT NULL IDENTITY (5,5),
+		Actor_fname VARCHAR(80) NOT NULL,
+		Actor_lname VARCHAR(80) NOT NULL,
+		Movie_name VARCHAR(80) NOT NULL
+	);
+	CREATE TABLE Table_Directorss (
+		Director_id INT PRIMARY KEY NOT NULL IDENTITY (50,1),
+		Director_fname VARCHAR(80) NOT NULL,
+		Director_lname VARCHAR(80) NOT NULL,
+		Actor_id INT NOT NULL CONSTRAINT fk_actors_Actor_id FOREIGN KEY REFERENCES Table_Actors(Actor_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	);
+	INSERT INTO Table_Actors
+			(Actor_fname, Actor_lname, Movie_Name)
+			VALUES
+			('Vijay', 'Malavika', 'Master'),
+			('Vijay', 'Keerthy Suresh', 'Sarkar'),
+			('Suriya', 'Anushka Shetty', 'Singam 3'),
+			('Suriya', 'Lijomol Jose', 'Jai Bhim'),
+			('Vijay Antony', 'Aathmika', 'Kodiyil Oruvan')
+	;
+	SELECT * FROM Table_Actors;
+	INSERT INTO Table_Directors
+			(Director_fname, Director_lname, Actor_id)
+			VALUES
+			('Lokesh', 'Kanagaraj', '1'),
+			('A.R','Murugadose', '2'),
+			('Hari', 'Raja', '3'),
+			('T.J', 'Gnanavel', '4'),
+			('Ananda', 'Krishnan', '5')
+	;
+	SELECT * FROM Table_Directors;
+	SELECT 
+		a1.Actor_fname, a1.Actor_lname, d1.Director_fname,
+		d1.Director_lname, a1.Movie_Name 
+		FROM Table_Actors a1
+		INNER JOIN Table_Directors d1 ON a1.Actor_id = d1.Actor_id
+		WHERE a1.Movie_Name = 'PK'
