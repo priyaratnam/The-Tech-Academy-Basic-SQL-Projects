15219eeea63b53dcf2dd62231b78a7eae7dd09a8
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2022-01-27 16:32:40 -0500

    Update School Database Assignment.sql

--- School Database Assignment/School Database Assignment.sql
+++ School Database Assignment/School Database Assignment.sql
@@ -1,82 +1,50 @@
-
+CREATE DATABASE School_Database
+GO
+USE School_Database
+GO
+CREATE TABLE Students(
+	Student_IDs INT PRIMARY KEY NOT NULL IDENTITY(2000,100),
+	Student_Names VARCHAR(50) NOT NULL
+	);
+INSERT INTO Students
+	(Student_Names)
+	VALUES
+	('Trishana Aran');
+;
+CREATE TABLE Instructors(
+	Instructor_IDs INT PRIMARY KEY NOT NULL IDENTITY(50000,1000),
+	Instructor_Names VARCHAR(50) NOT NULL,
+	);
+INSERT INTO Instructors
+	(Instructor_Names)
+	VALUES
+	('Kalaipriya Kanagaratnam'),
+	('Abishan Aran')
+;
+CREATE TABLE Classes(
+	Class_IDs INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+	Class_Names VARCHAR(50) NOT NULL,
+	);
+INSERT INTO Classes
+	(Class_Names)
+	VALUES
+	('Software and Web Development'),
+	('Computer and Programming')
+;
+UPDATE Students
+SET Instructor_ID_s = 58694
+WHERE Student_IDs = 784964
+;
+ALTER TABLE Students
+ADD Instructor_IDs INT CONSTRAINT fk_InstructorS_IDs FOREIGN KEY REFERENCES Instructors(Instructor_IDs);
+ALTER TABLE Classes
+ADD Instructor_IDs INT CONSTRAINT fk_Instructor_IDs FOREIGN KEY REFERENCES Instructors(Instructor_IDs);
+SELECT Instructor_Names FROM Instructors;
+SELECT Student_Names FROM Students ORDER BY Student_Names ASC;
 SELECT 
-    CONCAT(Class_ID, ', ', Class_Name) AS 'School:',
-    Instructure_ID AS 'Class_ID:',
-    CASE
-        WHEN Student_ID LIKE '%Manag%' THEN 'Student ID'
-        WHEN Student_Name LIKE '%Executive%' THEN 'Student Name'
-		WHEN Instructor_ID LIKE '%Chief%' THEN ' Instructor_ID'
-        WHEN Instructor_Name LIKE '%Chief%' THEN 'Instructor Name'
-        ELSE 'Staff'
-    END AS 'Worker Type:'
-    
-FROM
-    Name AS EM
-    JOIN class AS JO
-    ON Class_Name = Class_ID
-
-ORDER BY
-    'ClassID Type:' DESC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---////////////////////
---//////////// NEW! //
-
-
-
-
--- INSERT INTO
-INSERT INTO jobs
-VALUES('Thief', 25, 180)
-
-
-
-
-
-
-
-
-
-
-
-
--- UPDATE - problem!
-UPDATE jobs
-SET job_desc = 'Best Janitor'
-WHERE job_id = 15
-
-
-
-
-
-SELECT * FROM jobs
-WHERE job_id = 15
+	s1.Student_Names, i1.Instructor_Names, c1.Class_IDs
+	FROM Students s1, Instructors i1
+	INNER JOIN Classes c1 ON c1.Instructor_ID_s = i1.Instructor_IDs;
+SELECT * FROM Students;
+SELECT * FROM Instructors;
+SELECT * FROM Classes;
